e72647a5a2de489522fc5bff6b967cd5d69079d7
MaycolMD <maycolm@uninorte.edu.co>
2023-11-14 20:51:56 -0500

    better charts

--- graphql_flask/graphql/main.py
+++ graphql_flask/graphql/main.py
@@ -64,20 +64,20 @@ def obtener():
     # Extraer el diccionario de la respuesta GraphQL
     data_dict = result_.data['modelDpto'][0]
     dpto_value = data_dict.pop('dpto', None)
-    
+
     # Establecer un estilo de fondo blanco con bordes de las barras
     sns.set(style="whitegrid")
- 
+
     # Crear un gráfico de barras horizontal con un color más agradable
     fig, ax = plt.subplots(figsize=(12, 9))
-    
+
     # Convertir el diccionario en una Serie de Pandas
     dpto_importances = pd.Series(data_dict)
     dpto_importances.sort_values(ascending=True, inplace=True)
 
-    # Agregar etiquetas a las barras
+    # Formatear los valores en las barras
     for index, value in enumerate(dpto_importances):
-        ax.text(value, index, f'{value:.4f}', ha='left', va='center', fontweight='bold', fontsize=15, color='#333333')
+        ax.text(value, index, f'{value:.2%}', ha='left', va='center', fontweight='bold', fontsize=15, color='#333333')
 
     # Agregar etiquetas a los ejes y título
     plt.xlabel("Importancia", labelpad=20, weight='bold', size=20)
@@ -86,7 +86,12 @@ def obtener():
     ax.xaxis.grid(False)
     ax.yaxis.grid(False)
 
-    ax.barh(dpto_importances.index, dpto_importances.values, color='#86bf91', zorder=2)
+    # Utilizar una paleta de colores más atractiva
+    colors = sns.color_palette("Blues", len(dpto_importances))
+
+    # Crear el gráfico de barras con la nueva paleta de colores
+    ax.barh(dpto_importances.index, dpto_importances.values, color=colors, zorder=2, edgecolor='k', linewidth=1.5)
+
 
     # Quitar los bordes de la gráfica
     ax.spines['right'].set_visible(False)
@@ -97,9 +102,7 @@ def obtener():
     plt.yticks(range(len(dpto_importances)), dpto_importances.index)
 
     plt.savefig('./static/images/dpto.png', bbox_inches='tight', dpi=100)
-        # Convertir la figura de Matplotlib a un formato HTML que puede ser renderizado en el navegador
-    plot_html = mpld3.fig_to_html(fig)
-
+    # Convertir la figura de Matplotlib a un formato HTML que puede ser renderizado en el navegador
     plt.close(fig)
   
     return render_template('dpto.html', name = dpto_value, url ='/static/images/dpto.png')
