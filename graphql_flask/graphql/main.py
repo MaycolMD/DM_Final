075bda4ca7fa81ee6f86138dddeeabeacc908145
MaycolMD <maycolm@uninorte.edu.co>
2023-11-18 08:55:23 -0500

    Map fixed and details

--- graphql_flask/graphql/main.py
+++ graphql_flask/graphql/main.py
@@ -32,13 +32,85 @@ def mapa():
 
 @app.route('/dpto', methods=['GET'])
 def obtener():
+    departamentos = {
+        'Amazonas': 'Amazonas',
+        'Antioquia': 'Antioquia',
+        'Arauca': 'Arauca',
+        'Atlantico': 'Atlántico',
+        'Bolivar': 'Bolívar',
+        'Boyaca': 'Boyacá',
+        'Bogota D.C.': 'Bogotá, D.C.',
+        'Caldas': 'Caldas',
+        'Caqueta': 'Caquetá',
+        'Casanare': 'Casanare',
+        'Cauca': 'Cauca',
+        'Cesar': 'Cesar',
+        'Choco': 'Chocó',
+        'Cordoba': 'Córdoba',
+        'Cundinamarca': 'Cundinamarca',
+        'Guainia': 'Guainía',
+        'Guaviare': 'Guaviare',
+        'Huila': 'Huila',
+        'La Guajira': 'La Guajira',
+        'Magdalena': 'Magdalena',
+        'Meta': 'Meta',
+        'Narino': 'Nariño',
+        'Norte de Santander': 'Norte de Santander',
+        'Putumayo': 'Putumayo',
+        'Quindio': 'Quindío',
+        'Risaralda': 'Risaralda',
+        'San Andres': 'San Andrés y Providencia',
+        'Santander': 'Santander',
+        'Sucre': 'Sucre',
+        'Tolima': 'Tolima',
+        'Valle del Cauca': 'Valle del Cauca',
+        'Vaupes': 'Vaupés',
+        'Vichada': 'Vichada'
+    }
+
+    mapping = {
+        'AMAZONAS': 'Amazonas',
+        'ANTIOQUIA': 'Antioquia',
+        'ARAUCA': 'Arauca',
+        'ATLANTICO': 'Atlantico',
+        'BOLIVAR': 'Bolivar',
+        'BOYACA': 'Boyaca',
+        'SANTAFE DE BOGOTA D.C': 'Bogota D.C.',
+        'CALDAS': 'Caldas',
+        'CAQUETA': 'Caqueta',
+        'CASANARE': 'Casanare',
+        'CAUCA': 'Cauca',
+        'CESAR': 'Cesar',
+        'CHOCO': 'Choco',
+        'CORDOBA': 'Cordoba',
+        'CUNDINAMARCA': 'Cundinamarca',
+        'GUAINIA': 'Guainia',
+        'GUAVIARE': 'Guaviare',
+        'HUILA': 'Huila',
+        'LA GUAJIRA': 'La Guajira',
+        'MAGDALENA': 'Magdalena',
+        'META': 'Meta',
+        'NARINO': 'Narino',
+        'NORTE DE SANTANDER': 'Norte de Santander',
+        'PUTUMAYO': 'Putumayo',
+        'QUINDIO': 'Quindio',
+        'RISARALDA': 'Risaralda',
+        'SAN ANDRES': 'San Andres y Providencia',
+        'SANTANDER': 'Santander',
+        'SUCRE': 'Sucre',
+        'TOLIMA': 'Tolima',
+        'VALLE DEL CAUCA': 'Valle del Cauca',
+        'VAUPES': 'Vaupes',
+        'VICHADA': 'Vichada'
+    }
+
     dpto_value = request.args.get('departamento')
 
     print(dpto_value)
 
     query = f"""
     {{
-    modelDpto(dpto: "{dpto_value}") {{
+    modelDpto(dpto: "{mapping[dpto_value]}") {{
         dpto
         logroEducativo
         alfabetismo
@@ -73,15 +145,28 @@ def obtener():
 
     # Convertir el diccionario en una Serie de Pandas
     dpto_importances = pd.Series(data_dict)
+    dpto_importances.index = ['Bajo Logro Educativo', 'Analfabetismo',
+                                'Rezago Escolar',
+                                'Inasistencia Escolar',
+                                'Atención Integral a la Primera Infancia ',
+                                'Trabajo Infantil', 'Aseguramiento en salud ',
+                                'Barreras de acceso a salud', 'Tasa de Empleo Formal',
+                                'Desempleo de Larga Duración',
+                                'Acceso a fuente de agua mejorada ',
+                                'Inadecuado alcantarillado ',
+                                'Inadecuado material de pisos',
+                                'Inadecuado material de paredes exteriores',
+                                'Hacinamiento crítico']
     dpto_importances.sort_values(ascending=True, inplace=True)
 
     # Formatear los valores en las barras
     for index, value in enumerate(dpto_importances):
-        ax.text(value, index, f'{value:.2%}', ha='left', va='center', fontweight='bold', fontsize=15, color='#333333')
+        ax.text(value+0.004, index, f'{value:.2%}', ha='left', va='center', fontweight='bold', fontsize=16, color='#333333')
 
     # Agregar etiquetas a los ejes y título
-    plt.xlabel("Importancia", labelpad=20, weight='bold', size=20)
-    plt.ylabel("Característica", labelpad=20, weight='bold', size=20)
+    plt.xlabel("Importancia", labelpad=20, weight='bold', size=22)
+    plt.ylabel("Característica", labelpad=20, weight='bold', size=22)
+    plt.title(departamentos[dpto_value], weight='bold', size=26)
 
     ax.xaxis.grid(False)
     ax.yaxis.grid(False)
@@ -92,14 +177,13 @@ def obtener():
     # Crear el gráfico de barras con la nueva paleta de colores
     ax.barh(dpto_importances.index, dpto_importances.values, color=colors, zorder=2, edgecolor='k', linewidth=1.5)
 
-
     # Quitar los bordes de la gráfica
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['left'].set_visible(False)
 
     # Ajustar el espaciado entre las etiquetas
-    plt.yticks(range(len(dpto_importances)), dpto_importances.index)
+    plt.yticks(range(len(dpto_importances)), dpto_importances.index, fontsize=17)
 
     plt.savefig('./static/images/dpto.png', bbox_inches='tight', dpi=100)
     # Convertir la figura de Matplotlib a un formato HTML que puede ser renderizado en el navegador
--- graphql_flask/venv/Scripts/activate.bat
+++ graphql_flask/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\juanc\Documents\Universidad\Semestre 8\Minería de Datos\Final\DM_Final\graphql_flask\venv
+set VIRTUAL_ENV=D:\Python\DM_Final\graphql_flask\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
